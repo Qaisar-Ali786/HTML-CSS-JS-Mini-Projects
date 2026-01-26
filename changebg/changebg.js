72ae29fd4cb003fb350324236eb56bbafe56e038
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2026-01-26 01:24:07 -0800

    Change background

---
+++ changebg/changebg.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById("btn");
+const hex = document.getElementById("hexCode");
+function randomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 6)];
+  }
+
+  return color;
+}
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = randomColor();
+  hex.innerHTML = randomColor();
+});
